1fcfa92a059efe2f2bdc27499ba1e3470b5f6960
Kristian Nilsen <github@kristiannilsen.no>
2024-12-17 08:39:46 +0100

    feat: Add generation of release notes

---
+++ src/main/java/com/example/release_please/ReleasePleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.release_please;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/release_please/ReleasePleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.release_please;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasePleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
